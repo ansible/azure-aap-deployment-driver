be05fe10a089eecfdc7237cd7675c58325a756b7
Bryan Havenstein <bhavenst@redhat.com>
2023-02-27 17:09:53 -0700

    Patternfly Table

--- ui/src/app/ErrorInfo/ErrorInfo.tsx
+++ ui/src/app/ErrorInfo/ErrorInfo.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Popover, Icon, Tooltip } from '@patternfly/react-core';
+import { TableComposable, Tr, Th, Td, OuterScrollContainer } from '@patternfly/react-table'
 import ExclamationCircleIcon from '@patternfly/react-icons/dist/esm/icons/exclamation-circle-icon';
 import { DeploymentStepStatusData } from '@app/apis/types';
+
+import './ErrorInfo.css'
 
 interface IErrorInfoProps {
   stepStatusData: DeploymentStepStatusData;
 }
 
 export const ErrorInfoPopover = ({ stepStatusData }: IErrorInfoProps) => {
-  const borders = {
-    border: '1px solid rgb(0,0,0)',
-  };
 
-  let data = (
+  const data = (
     <>
-      <table>
-        <tr style={borders}>
-          <th>Error:</th>
-          <td>{stepStatusData.error}</td>
-        </tr>
-        <tr style={borders}>
-          <th>Details:</th>
-          <td>
-            <div style={{ height: '20rem', overflowY: 'scroll', whiteSpace: 'pre-wrap' }}>{stepStatusData.errorDetails}</div>
-          </td>
-        </tr>
-        <tr style={borders}>
-          <th style={{width: "8rem"}}>Correlation ID:</th>
-          <td>{stepStatusData.correlationId}</td>
-        </tr>
-      </table>
+    <TableComposable>
+        <Tr>
+            <Th>Error:</Th>
+            <Td className='errorCell'>{stepStatusData.error}</Td>
+        </Tr>
+        <Tr>
+            <Th>Details:</Th>
+            <Td className='errorDetailCell'><div className='errorDetailDiv'>{stepStatusData.errorDetails}</div></Td>
+        </Tr>
+        <Tr>
+            <Th modifier='nowrap'>Correlation ID:</Th>
+            <Td>{stepStatusData.correlationId}</Td>
+        </Tr>
+    </TableComposable>
     </>
   );
   return (
