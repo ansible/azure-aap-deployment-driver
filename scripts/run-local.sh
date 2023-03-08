78fea9340666666aa3b4fd6caf29108d12ff3979
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-03-08 13:53:04 -0500

    removing env setup

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,33 +1,9 @@
 #!/usr/bin/env bash
 
-server_local_development_env () {
-    # export / set all environment variables from the local development env defaults in /configs
-    echo "Copying environment .env from configs/.env.development.local"
-    cp ./configs/.env.development.local ./build/.env
-
-    # Azure environment variables
-    if command -v az &> /dev/null
-    then
-        echo "Azure CLI found. Setting Azure environment variables using 'az account show'."
-        export AZURE_TENANT_ID=$(az account show --query tenantId -o tsv)
-        export AZURE_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
-    fi
-
-    # ensure we have a resource group (we are assuming az login has occurred)
-    local $(cat ./configs/.env.development.local | grep 'RESOURCE_GROUP_NAME' | xargs)
-
-    echo "Ensuring resource group '${RESOURCE_GROUP_NAME}'"
-    az group create -n $RESOURCE_GROUP_NAME -l eastus2 -o none
-
+run_server () {
     # local sqlite database so it has a db to run
     touch "build/${DB_PATH}"
-    echo ""
-}
-
-run_server () {
-    # setup local environment variables and start the server
-    server_local_development_env
-    echo -e "Starting Server.\n" 
+    echo -e "\nStarting Server.\n" 
     pushd ./build
         ./server
     popd
