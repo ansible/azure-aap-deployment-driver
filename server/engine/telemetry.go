5da8a25f1065656e2b7886aa5ebd8f9e0d1effc4
Viraj <26265354+Viraj27@users.noreply.github.com>
2023-04-19 08:55:00 -0700

    Add Region, AccessType and Deployment End Time metrics to Telemetry (#44)
    * Initial commit for creating a segment client
    
    * Minor tweaks to telemetry.go
    
    * Made publisher generic - added write key to config
    
    * Added logs to test Segment publisher
    
    * ApplicationId added in properties and property naming in lowercase
    
    * propertyMap populated directly from DB
    
    * check for segment key env var and if not present, do nothing
    
    * Check for rogue or missing event names before sending to Segment
    
    * Moved telemetry.go to engine package and removed test from model_test
    
    * Updated go dependencies and updated PR comments
    
    * Update warn message
    
    * Add Accesstype and Region to metrics
    
    * Remove eventname hardcode
    
    * Add deployment end time metric
    
    * Changes added as per review comments
    
    * removed comments and changed to log.Error

--- server/engine/telemetry.go
+++ server/engine/telemetry.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"server/config"
 	"server/model"
+	"time"
 
 	"github.com/segmentio/analytics-go/v3"
 	log "github.com/sirupsen/logrus"
@@ -34,6 +35,23 @@ func BuildSegmentPropertiesMap(db *gorm.DB) analytics.Properties {
 	return propertiesMap
 }
 
+func StoreMetricFromMainOutputs(db *gorm.DB) {
+
+	var mainOutput model.Output
+	db.Where("module_name = ?", "").Find(&mainOutput)
+
+	if loc, exists := mainOutput.Values["location"]; exists {
+		model.SetMetric(db, model.Region, loc.(map[string]interface{})["value"].(string))
+	} else {
+		log.Error("Location of deployment is missing : will not be included in telemetry")
+	}
+	if access, exists := mainOutput.Values["access"]; exists {
+		model.SetMetric(db, model.AccessType, access.(map[string]interface{})["value"].(string))
+	} else {
+		log.Error("Access Type of deployment is missing : will not be included in telemetry")
+	}
+}
+
 func PublishToSegment(db *gorm.DB) {
 
 	writeKey := config.GetEnvironment().SEGMENT_WRITE_KEY
@@ -43,6 +61,9 @@ func PublishToSegment(db *gorm.DB) {
 	}
 	// set metrics in DB that are not set yet
 	model.SetMetric(db, model.ApplicationId, config.GetEnvironment().APPLICATION_ID)
+	// time.RFC3339 format is the Go equivalent to ISO 8601 format (minus the milliseconds)
+	model.SetMetric(db, model.EndTime, time.Now().Format(time.RFC3339))
+	StoreMetricFromMainOutputs(db)
 	//gather all metrics in a property map
 	propertiesMap := BuildSegmentPropertiesMap(db)
 	eventName := GetEvent(propertiesMap)
