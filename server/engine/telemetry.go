bcd10ec4c2cd6113a64a47b7cb4c5148b4fdda44
Bruno Sanchez <58506651+brsanche@users.noreply.github.com>
2023-04-28 16:54:03 +0100

    adding StartTime metric to Telemetry (#46)
    * adding StartTime metric to Telemetry
    
    * adding environment and changing how to get the starttime
    
    * Add number of retries and error details for each step as a metric to telemetry (#45)
    
    * Initial commit for creating a segment client
    
    * Minor tweaks to telemetry.go
    
    * Made publisher generic - added write key to config
    
    * Added logs to test Segment publisher
    
    * ApplicationId added in properties and property naming in lowercase
    
    * propertyMap populated directly from DB
    
    * check for segment key env var and if not present, do nothing
    
    * Check for rogue or missing event names before sending to Segment
    
    * Moved telemetry.go to engine package and removed test from model_test
    
    * Updated go dependencies and updated PR comments
    
    * Update warn message
    
    * Add Accesstype and Region to metrics
    
    * Remove eventname hardcode
    
    * Add deployment end time metric
    
    * Changes added as per review comments
    
    * removed comments and changed to log.Error
    
    * Add number of retries for each step as a metric
    
    * Add no.of retries and errors per each step of deployment to telemetry
    
    * Updated as per PR comments
    
    * Updated function naming as per PR comment
    
    * changing to not set startitme & appID if empty
    
    * adding default starttime
    
    * changing warning
    
    ---------
    
    Co-authored-by: Viraj <26265354+Viraj27@users.noreply.github.com>

--- server/engine/telemetry.go
+++ server/engine/telemetry.go
@@ -124,6 +124,17 @@ func PublishToSegment(db *gorm.DB) {
 		log.Errorf("Segment Write Key is missing : Not sending telemetry to Segment")
 		return
 	}
+	// set metrics in DB that are not set yet
+	if config.GetEnvironment().APPLICATION_ID != "" {
+		model.SetMetric(db, model.ApplicationId, config.GetEnvironment().APPLICATION_ID)
+	}
+	// time.RFC3339 format is the Go equivalent to ISO 8601 format (minus the milliseconds)
+	model.SetMetric(db, model.EndTime, time.Now().Format(time.RFC3339))
+	// the time is being retrieved as utcNow from the bicep script ( ISO 8601)
+	if config.GetEnvironment().START_TIME != "" {
+		model.SetMetric(db, model.StartTime, config.GetEnvironment().START_TIME)
+	}
+	StoreMetricFromMainOutputs(db)
 	//gather all metrics in a property map
 	propertiesMap := BuildSegmentPropertiesMap(db)
 	eventName := GetEvent(propertiesMap)
