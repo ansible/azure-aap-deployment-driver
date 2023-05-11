b274eff7825d58828052b1b0b47813a1ff998f13
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-11 12:12:01 -0400

    fixing entrypoint and image in compose file

--- package/entrypoint.sh
+++ package/entrypoint.sh
@@ -7,11 +7,11 @@ echo "Server starting."
 
 echo "MODM - API Server starting."
 # Start the api server
-/modm/apiserver /dev/fd/1 2>&1 &
+/apiserver /dev/fd/1 2>&1 &
 
 echo "MODM - Operator starting."
 # Start the operator server
-/modm/operator /dev/fd/1 2>&1 &
+/operator /dev/fd/1 2>&1 &
 
 # Wait for any process to exit
 wait -n
