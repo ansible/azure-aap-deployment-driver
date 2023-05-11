58b67b19bb266288288aba9e7804af133c495751
Kevin Hillinger <kevin.hillinger@microsoft.com>
2023-05-11 12:59:23 -0400

    updating run local script

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -18,7 +18,28 @@ run_ui () {
 
 run_docker() {
    ./build.sh docker
+   start_ngrok_background
+   docker compose -f ./tools/docker-compose.yml up  
 }
+
+function start_ngrok_background() {
+  # start up ngrok and get address
+  ngrok http 8080 > /dev/null &
+  ngrok_start_result=$?
+  export NGROK_ID=$!
+
+  if [ $ngrok_start_result -gt 0 ]; then
+    echo "NGROK failed to start."
+    echo "exiting."
+    exit 1
+  fi
+
+  echo "NGROK started: $NGROK_ID"
+  sleep 2
+  export MODM_PUBLIC_BASE_URL=$(curl -s localhost:4040/api/tunnels | jq '.tunnels[0].public_url' -r)
+  echo "NGROK URL:  $MODM_PUBLIC_BASE_URL"
+}
+
 
 TARGET=$1
 
@@ -29,6 +50,9 @@ case $TARGET in
     ;;
   ui)
     run_ui
+    ;;
+  docker)
+    run_docker
     ;;
   *)
     echo -n "unknown"
