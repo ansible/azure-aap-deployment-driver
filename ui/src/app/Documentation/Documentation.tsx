51ef3307dddb63db95e8ea1dbd13ff2d62fce589
Tomas Z <tznamena@redhat.com>
2023-07-13 15:53:28 +0200

    Update documentation page

--- ui/src/app/Documentation/Documentation.tsx
+++ ui/src/app/Documentation/Documentation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CubesIcon } from '@patternfly/react-icons';
+import { CubesIcon, ExternalLinkSquareAltIcon } from '@patternfly/react-icons';
 import {
   PageSection,
   Title,
@@ -16,17 +16,19 @@ export const Documentation = () => {
       <EmptyState variant={EmptyStateVariant.full}>
         <EmptyStateIcon icon={CubesIcon} />
         <Title headingLevel="h1" size="lg">
-          Ansible Automation Platform on Azure documentation
+          Documentation
         </Title>
-        <EmptyStateBody>(opens in a new tab)</EmptyStateBody>
-        <Button
-          component="a"
-          href="https://access.redhat.com/documentation/en-us/ansible_on_clouds/2.x/html/red_hat_ansible_automation_platform_on_microsoft_azure_guide/index"
-          target="_blank"
-          variant="primary"
-        >
-          AAP on Azure Guide
-        </Button>{' '}
+        <EmptyStateBody>
+          <Button
+            component="a"
+            href="https://access.redhat.com/documentation/en-us/ansible_on_clouds/2.x/html/red_hat_ansible_automation_platform_on_microsoft_azure_guide/index"
+            target="_blank"
+            variant="link"
+            icon={<ExternalLinkSquareAltIcon />}
+            iconPosition="right">
+            Red Hat Ansible Automation Platform on Microsoft Azure Guide
+          </Button>
+        </EmptyStateBody>
       </EmptyState>
     </PageSection>
   );
