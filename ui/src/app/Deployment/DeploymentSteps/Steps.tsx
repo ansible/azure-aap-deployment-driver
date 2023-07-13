1b48946e325b934790213a1f30e636604813b9e3
Tomas Z <93541722+tznamena@users.noreply.github.com>
2023-07-13 22:24:12 +0200

    Add info about sw subscription on deployment (#69)
    * Update node.js
    
    * Add alert about sw subscription
    
    * Update ui/src/app/Deployment/DeploymentInfo.tsx
    
    Co-authored-by: Aine Riordan <44700011+ariordan-redhat@users.noreply.github.com>
    
    * Store flag for showing deployment info in browser session
    
    * Updates based on PR review
    
    * Fix typo in comment
    
    * Update documentation page
    
    ---------
    
    Co-authored-by: Aine Riordan <44700011+ariordan-redhat@users.noreply.github.com>

--- ui/src/app/Deployment/DeploymentSteps/Steps.tsx
+++ ui/src/app/Deployment/DeploymentSteps/Steps.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bullseye, Stack, StackItem, PageSection, PageSectionVariants, TextContent, Text, Title, List } from '@patternfly/react-core';
 import { DeploymentStep } from "./Step";
 import { DeploymentStepData } from '@app/apis/types';
 
 import './Steps.css'
+import { DeploymentInfo } from '../DeploymentInfo';
 
 interface IDeploymentStepsProps {
   stepsData: DeploymentStepData[]
@@ -11,6 +12,31 @@ interface IDeploymentStepsProps {
 
 
 export const DeploymentSteps = ({ stepsData }: IDeploymentStepsProps, ) => {
+
+  // Persist this value browser's session (each tab has its own)
+  const [showDeploymentInfo, setShowDeploymentInfo] = useState<Boolean>(()=>{
+    let storageItem = sessionStorage.getItem('showDeploymentInfo');
+    if (storageItem === null) {
+      // store initial value of true and return it
+      sessionStorage.setItem('showDeploymentInfo',String(true));
+      return true;
+    } else {
+      // return stored value
+      return storageItem.toLowerCase() === 'true'
+    }
+  })
+
+  useEffect(()=>{
+    // update the value in browser session when changed
+    sessionStorage.setItem('showDeploymentInfo',String(showDeploymentInfo));
+  },[showDeploymentInfo])
+
+
+
+  const closeDeploymentInfo = () => {
+    setShowDeploymentInfo(!showDeploymentInfo);
+  }
+
   return (
     <>
       <PageSection variant={PageSectionVariants.light}>
@@ -18,6 +44,7 @@ export const DeploymentSteps = ({ stepsData }: IDeploymentStepsProps, ) => {
           <Text component="h1">Ansible Automation Platform Deployment Engine</Text>
         </TextContent>
       </PageSection>
+      {showDeploymentInfo && <DeploymentInfo closeHandler={closeDeploymentInfo}/>}
       <PageSection>
         <Bullseye>
           <Stack hasGutter className='deploymentStepsCont'>
