83d2ce199ceda42f8e83016ebe0ccf3c1c6e0926
Tomas Z <tznamena@redhat.com>
2023-12-19 12:28:37 -0500

    Add unit tests for logo

---
+++ ui/src/app/AppLayout/__tests__/AppLayout.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { AppLayout } from '../AppLayout';
+
+describe('AppLayout', ()=>{
+	it('renders Page with proper AAP logo', ()=> {
+		const routes = [
+			{
+				element: <AppLayout navigation={[]} />,
+				children: [
+					{
+						path: "/",
+						element: <></>
+					}
+				]
+			}
+		]
+
+		const router = createMemoryRouter(routes, {
+			initialEntries: ["/"],
+		});
+		// what's passed to the render() is the same what's done index.tsx file
+		render(<RouterProvider router={router} />);
+
+		//screen.debug();
+
+		const logo = screen.getByAltText("Red Hat Ansible Automation Platform Logo")
+		expect(logo).toBeInTheDocument()
+		expect(logo).toBeVisible()
+		expect(logo).toHaveAttribute("src","Technology_icon-Red_Hat-Ansible_Automation_Platform-Standard-RGB.svg")
+	})
+})
