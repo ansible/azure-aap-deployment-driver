6be6b5cbd124da71c4321b7cec8526f765cfd54a
Tomas Z <93541722+tznamena@users.noreply.github.com>
2024-01-16 21:50:03 +0100

    AAP-19533 Fix rendering of the Red Hat Login dialog title (#109)
    * AAP-19533 Fix rendering of RH login modal title
    
    * Fix formatting

--- ui/src/app/Deployment/RHLoginModal.tsx
+++ ui/src/app/Deployment/RHLoginModal.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Button, Modal, ModalVariant } from '@patternfly/react-core';
+import { Button, Icon, Modal, ModalVariant, Title, TitleSizes } from '@patternfly/react-core';
 import ExternalLinkSquareAltIcon from '@patternfly/react-icons/dist/esm/icons/external-link-square-alt-icon';
+import ExclamationTriangleIcon from '@patternfly/react-icons/dist/esm/icons/exclamation-triangle-icon';
+import './RHLoginModal.css';
 
 interface IRHLoginProps {
 	isModalShown: boolean
 }
 
+const DialogHeader = <Title headingLevel="h1" size={TitleSizes['2xl']}>
+	<Icon status="warning" isInline><ExclamationTriangleIcon /></Icon>A valid subscription for Ansible Automation Platform in your Red Hat account is required
+</Title>;
+
 export const RHLoginModal = ({isModalShown}: IRHLoginProps) => {
 	return (
 		<Modal
-        title="A valid subscription for Ansible Automation Platform in your Red Hat account is required"
-				titleIconVariant="warning"
-        isOpen={isModalShown}
-        showClose={false}
-				variant={ModalVariant.medium}
-				actions={[
-					// TODO Set the URL in href below programmatically
-          <Button
-					key="login" variant="primary"
-					icon={<ExternalLinkSquareAltIcon />} iconPosition="right"
-					component="a" href="/sso" target="_self">Log in with Red Hat account</Button>,
-        ]}
+			header={DialogHeader}
+			isOpen={isModalShown}
+			showClose={false}
+			variant={ModalVariant.medium}
+			actions={[
+				// TODO Set the URL in href below programmatically
+				<Button
+				key="login" variant="primary"
+				icon={<ExternalLinkSquareAltIcon />} iconPosition="right"
+				component="a" href="/sso" target="_self">Log in with Red Hat account</Button>,
+			]}
       ><p>Your Ansible Automation Platform deployment is underway.</p>
 			<br /><p>To use Ansible Automation Platform on Azure, you MUST have a valid subscription for Ansible Automation Platform in your Red Hat account.</p>
 			<br />
