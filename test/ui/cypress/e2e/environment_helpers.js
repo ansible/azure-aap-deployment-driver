cfa700148bd10b724ca95e076bec6423e7c09fd9
Qi Wang <qwa@redhat.com>
2024-03-25 16:57:20 -0400

    update to address review comments

--- test/ui/cypress/e2e/environment_helpers.js
+++ test/ui/cypress/e2e/environment_helpers.js
@@ -1,10 +1,25 @@
 export function verifyRequiredEnvVariables() {
-	let deploymentDriverUrl = Cypress.env('DEPLOYMENT_DRIVER_URL')
-  let username = Cypress.env('USERNAME')
-  let password = Cypress.env('PASSWORD')
-  expect(deploymentDriverUrl).not.to.be.undefined
-  // uncomment following when we implement SSO login
-  //expect(username).not.to.be.undefined
-  //expect(password).not.to.be.undefined
-	return { deploymentDriverUrl, username, password }
+
+  if (!Cypress.env('DEPLOYMENT_DRIVER_URL')) {
+    throw new Error(`Missing required environment variable for DEPLOYMENT_DRIVER_URL`);
+  }
+
+  if (!Cypress.env('DEPLOYMENT_ENGINE_UI_PASSWORD')) {
+    throw new Error('Missing required environment variable for DEPLOYMENT_ENGINE_UI_PASSWORD');
+  }
+
+  if (!Cypress.env('RH_SSO_URL')) {
+    throw new Error('Missing required environment variable for RH_SSO_URL');
+  }
+
+  if (!Cypress.env('RH_ACCOUNT_USERNAME')) {
+    throw new Error('Missing required environment variable for RH_ACCOUNT_USERNANE');
+  }
+
+  if (!Cypress.env('RH_ACCOUNT_PASSWORD')) {
+    throw new Error('Missing required environment variable for RH_ACCOUNT_PASSWORD');
+
+  }
+
+	return 
 }
