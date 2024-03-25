e3c5ac6468f32c200f1d07bdb2dd0f2293e0b5dc
Qi Wang <qwa@redhat.com>
2024-03-25 19:01:26 -0400

    update to address review comments

--- test/ui/cypress/e2e/deployment_driver.cy.js
+++ test/ui/cypress/e2e/deployment_driver.cy.js
@@ -1,5 +1,3 @@
-import { verifyRequiredEnvVariables } from './environment_helpers'
-
 /// <reference types="cypress" />
 
 describe('Deployment driver web UI', () => {
@@ -13,38 +11,101 @@ describe('Deployment driver web UI', () => {
     cy.get('@main').contains('Deployment Steps')
     cy.get('@main').get('div.deployProgress').as('statusArea')
     cy.get('@statusArea').get('button.cancelButton').contains('Cancel Deployment')
+
+    cy.get('.pf-c-brand').should('be.visible')
+    cy.get('.pf-c-alert__title')
+      .contains('You currently have a subscription to Ansible Automation Platform')
+    cy.get('p').contains('To manage or setup new subscription, visit the')
+    cy.get('p > .pf-c-button')
+      .contains('Red Hat Hybrid Cloud Console')
+      .should('have.attr', 'href', 'https://console.redhat.com/')
+
+    // Check the H1 title
+    cy.get('h1')
+      .contains('Ansible Automation Platform Deployment Engine')
+      .should('be.visible') 
   })
 
   it('left navigation has expected navigation links', () => {
+    cy.get('#nav-toggle').click()
     cy.get('div#page-sidebar').as('navigation')
+
     cy.get('@navigation').contains('Deployment')
+      .should('be.visible')
+      .should('have.attr','href').and('include','/')
+
     cy.get('@navigation').contains('Documentation')
+      .should('be.visible')
+      .should('have.attr','href').and('include', '/documentation')
+
     cy.get('@navigation').contains('Logout')
+      .should('be.visible')    
   })
 
   it('list of deployment steps contains expected steps', () => {
-    cy.get('div.deploy-step>ul').as('steps')
-    cy.get('@steps').contains('VNET and Subnets')
-    cy.get('@steps').contains('Private DNS')
-    cy.get('@steps').contains('AAP Repository')
-    cy.get('@steps').contains('Database Server and Databases')
-    cy.get('@steps').contains('AKS Cluster')
-    cy.get('@steps').contains('AAP Operators')
-    cy.get('@steps').contains('AAP Applications')
-    cy.get('@steps').contains('Application Ingress')
-    cy.get('@steps').contains('Seeded Content')
-    cy.get('@steps').contains('Billing')
+    // Check the title of Deployment Steps
+    cy.get('.pf-c-title')
+      .contains('Deployment Steps')
+      .should('be.visible')
+
+    // Check the title of Deployment Steps
+    cy.get('.pf-c-title')
+      .contains('Deployment Steps')
+      .should('be.visible')
+    
+    // Check the detailed deployment step's names in the Deployment Steps panel
+    cy.fixture('deployment_steps.json').then((deploymentStepsData) => {
+      let total_steps = deploymentStepsData.steps.length
+      cy.log('Total Deployment Steps: '+total_steps)
+      
+      cy.get('div.deploy-step>ul').as('steps')
+      for (let i=0; i<total_steps; i++) {
+        cy.log('Deployment Step '+ deploymentStepsData.steps[i].stepNo +": " + deploymentStepsData.steps[i].Name)
+        
+        cy.get('@steps')
+          .contains(`${deploymentStepsData.steps[`${i}`].Name}`)
+      }
+    })
+
   })
 
   it('clicking cancel button brings up a dialog that can be closed', () => {
+    // Check the Cancel Deployment button on the main screen
     cy.get('div.deployProgress').as('statusArea')
     cy.get('@statusArea').get('button.cancelButton').click()
+
+    // Check the UI elements on the pupped up dialog after clicking the Cancel Deployment button from the main screen
     cy.get('div[role="dialog"]').as('dialog')
-    cy.get('@dialog').contains('Cancel Deployment')
+    cy.get('@dialog', {timeout: 4000}).contains('Cancel Deployment')
     cy.get('@dialog').get('button.pf-m-primary').contains('Confirm')
     cy.get('@dialog').get('button.pf-m-link').as('closeButton')
-    cy.get('@closeButton').contains('No')
+    cy.get('@closeButton').contains('No').should('not.be.selected')
     cy.get('@closeButton').click()
-    cy.get('@dialog').should('not.exist')
+    cy.get('@dialog').should('not.exist')   
+  })
+
+  it('Checking the Deployment Engine UI logout', () => {
+    // Select the Logout from the navigation menu  
+  cy.get('#nav-toggle').click()
+  cy.get('#Logout-2').click()
+
+  //Check the messages on the popped up Logout screen
+  cy.get('.pf-c-modal-box__title-text')
+    .contains('Logout')
+    .should('be.visible')
+  cy.get('#pf-modal-part-3')
+    .contains('Are you sure you want Logout?')
+    .should('be.visible')
+ 
+  // Check the 'Cancel' button is unselected
+  cy.get('.pf-c-modal-box__footer > .pf-m-link')
+    .contains('Cancel')
+    .should('not.be.selected')
+
+  //Check the 'Confirm' button is selected 
+  cy.get('button#primary-loading-button.pf-c-button.pf-m-primary.pf-m-progress', {timeout: 4000})
+    .contains('Confirm')
+    .should('be.visible')
+    .click()
   })
 })
