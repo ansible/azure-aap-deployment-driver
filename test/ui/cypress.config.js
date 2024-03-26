38c31605c1155f40b634957af00e498de8d35b75
Qi Wang <qwa@redhat.com>
2024-03-26 13:16:38 -0400

    update cypress.config.ts

--- test/ui/cypress.config.js
+++ test/ui/cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  // These settings apply everywhere unless overridden
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
